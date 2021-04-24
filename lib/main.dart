e474b15a5d0ee06b729fde164cf4345c46a14086
amittiwari04 <amittiwarimom@gmail.com>
2021-04-24 18:13:43 +0530

    master

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:WhatsApp/splash_screen/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
